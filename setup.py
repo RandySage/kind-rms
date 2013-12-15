50249c20fb04d5c87cd0e28721e0cb0d3c3d4119
NA <NA>
2013-12-15 14:24:05 -0800

    Hack to setup.py; used to install remotely...

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-from grind.settings import VERSION
-DESCRIPTION = ("REST API for basic SQL interaction")
+import subprocess
 
-setup(
-    name='GRIND',
-    version=VERSION,
-    description=DESCRIPTION,
-    url='https://github.com/ransage/grind',
-    packages=['grind'],
-    install_requires=['setuptools','eve'],
-    tests_require=['nose'],
-    test_suite='src'
-)
+try:
+    with open('./env/bin/pip'):
+        subprocess.call("./env/bin/pip install -r requirements.txt",shell=True)
+except IOError:
+    print "Unable to pen ./env/bin/pip - did you run:"
+    print "  virtualenv env"
+    print "???"
+
+
+
+print "This should be cleaned up to follow standard paradigms"
+
+# from setuptools import setup, find_packages
+# DESCRIPTION = ("DOORS'-style Requirements Management System - KIND Is Not DOORS")
+
+# setup(
+#     name='KIND',
+#     version='0.0-dev',
+#     description=DESCRIPTION,
+#     url='https://github.com/ransage/kind',
+#     packages=['django_project'],
+#     install_requires=['setuptools'],
+#     tests_require=['nose'],
+#     test_suite='src'
+# )
